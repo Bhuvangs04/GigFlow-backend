59896bbc456aa2292f6244c0fcf6c67cd06ede42
unkown-prog <bhuvi4662@gmail.com>
2026-01-13 17:41:45 +0530

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080",
+        origin: "https://gig-flow-frontend-orcin.vercel.app",
         credentials: true
     }
 });
